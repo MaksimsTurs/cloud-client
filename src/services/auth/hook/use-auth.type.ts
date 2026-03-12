1bc52dafc1d58fb0929537b6bc2f3365f99b174b
MaksimsTurs <maksims07turs@gmail.com>
2026-03-12 16:16:44 +0100

    refactor(service/auth): update jsdoc, rename types for returning functions

--- src/services/auth/hook/use-auth.type.ts
+++ src/services/auth/hook/use-auth.type.ts
@@ -1,38 +1,34 @@
 import type { AuthTokens } from "../components/Auth-Provider/Auth-Provider.type";
 
 export type UseAuthReturn<E = unknown> = {
-  isAuthorizing: boolean
   /**
-    *  @description Loading state.
-    */
-  isLoading: boolean
-  /**
-    *  @description Serialized error that occur while `withAuth` function
-    *  execution.
-    */
-  error?: E
-  /**
-    *  @description Gives true if user is authorized, to get authorized, the client should
-    *  have valid access token, if you loged up you can call `authorize` function that will 
-    *  make request to you server to get tokens, otherwise you
-    *  should create a new account or log in.
+    * @description indicator if the user is authorized, returning a true if both access and refresh tokens are defined.
     */
   isAuthorized: boolean
   /**
-    @description make request to you server to get access token.
-  */
-  authorize: UseAuthAuthorize
+    * @description indicator if the authorization is running.
+    */
+  isAuthorizing: boolean
   /**
-    @description make log in/up request to you server, callback should return a 
-    access token.
-  */
-  authenticate: UseAuthAuthenticate
+    * @description indicator for local loading state.
+    */
+  isLoading: boolean
   /**
-    @description make log out request to you server and
-    remove all local information, server should clear http cookie.
-    When user are loged out function return two undefineds, for data and error.
-  */
-  logout: UseAuthLogout
+    * @description serialized error.
+    */
+  error?: E
+  /**
+    * @description make initial request to you server to get access and refresh tokens.
+    */
+  authorize: Authorize
+  /**
+    * @description make log in or log up request to you server to get access and refresh tokens.
+    */
+  authenticate: Authenticate
+  /**
+    * @description make log out request to you server and remove all local information, server should clear http cookies.
+    */
+  logout: Logout
 };
 
 export type UseAuthOptions<E = unknown> = {
@@ -49,10 +45,10 @@ type SerializeError<E = unknown> = (error: unknown) => E;
 
 // Functions
 
-type UseAuthCallback = () => Promise<UseAuthEndpointResponse | undefined>;
+type AuthCallback = () => Promise<UseAuthEndpointResponse | undefined>;
 
-type UseAuthAuthorize = (callback: UseAuthCallback) => Promise<boolean>;
+type Authorize = (callback: AuthCallback) => Promise<boolean>;
 
-type UseAuthLogout = (callback: UseAuthCallback) => Promise<boolean>;
+type Logout = (callback: AuthCallback) => Promise<boolean>;
 
-type UseAuthAuthenticate = (callback: UseAuthCallback) => Promise<boolean>;
+type Authenticate = (callback: AuthCallback) => Promise<boolean>;
