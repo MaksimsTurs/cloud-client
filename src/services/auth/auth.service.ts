cd2d81c8876a555eee8018d992faa0d178833d88
MaksimsTurs <maksims07turs@gmail.com>
2026-03-14 20:17:35 +0100

    refactor: add imports for new hooks and components

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import useUser from "./hooks/use-user.hook";
 import useAuth from "./hooks/use-auth.hook";
 import useWithAuth from "./hooks/use-with-auth.hook";
 import useAuthIsAuthorized from "./hooks/use-auth-is-authorized.hook";
@@ -7,6 +8,7 @@ import AuthRoute from "./components/Auth-Route/Auth-Route.component";
 import AuthProvider from "./components/Auth-Provider/Auth-Provider.component";
 
 export {
+  useUser,
   useAuth,
   useWithAuth,
   useAuthIsAuthorized,
