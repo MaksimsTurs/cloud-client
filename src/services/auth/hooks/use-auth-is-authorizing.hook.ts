1018aa296e92e56ff6629921f7742b544d335659
MaksimsTurs <maksims07turs@gmail.com>
2026-03-14 20:12:21 +0100

    feat: add hook use-auth-is-authorizing

--- src/services/auth/hooks/use-auth-is-authorizing.hook.ts
+++ src/services/auth/hooks/use-auth-is-authorizing.hook.ts
@@ -4,11 +4,13 @@ import { useContext } from "react";
 
 import { AuthContext } from "../components/Auth-Provider/Auth-Provider.component";
 
+import { isUndefined } from "@util/is.util";
+
 export default function useAuthIsAuthorizing(): boolean {
   const context: AuthContextValue | undefined = useContext<AuthContextValue | undefined>(AuthContext);
 
-  if(!context) {
-    throw new Error("Wrap you'r application into AuthProvider compontent!");
+  if(isUndefined(context)) {
+    throw new TypeError("Wrap you'r application into AuthProvider compontent!");
   }
 
   return context.isAuthorizing;
