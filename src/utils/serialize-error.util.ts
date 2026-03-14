47624100c93286b3d5a6a31d08dbcf1cc2217d2b
MaksimsTurs <maksims07turs@gmail.com>
2026-03-14 20:18:13 +0100

    refactor: add serializion for errors that come from http

--- src/utils/serialize-error.util.ts
+++ src/utils/serialize-error.util.ts
@@ -1,14 +1,34 @@
-import { isInstanceOf, isObject } from "./is.util";
-import { inObject } from "./std/std.util";
+import type { SerializedError } from "@root/global.type";
 
-export default function serializeError(error: unknown) {
-  if(isInstanceOf(error, Error)) {
+import HTTPError from "./http/errors/HTTP-Error.error";
+
+import { isInstanceOf, isObject } from "./is.util";
+
+export default async function serializeError(error: unknown): Promise<SerializedError> {
+  if(isInstanceOf(error, HTTPError)) {
+    const contentType: string | null = error.response.headers.get("Content-Type");
+
+    if(isJson(contentType)) {
+      const json = await error.response.json();
+      return { code: json.code, message: json.message };
+    }
+
+    return { message: "Unknown server error!" };
+  } else if(isInstanceOf(error, Error)) {
     return { message: error.message };
   } else if(isObject(error)) {
-    if(inObject(["message", "code"], error)) {
+    if(Object.hasOwn(error, "message") && Object.hasOwn(error, "code")) {
       return { message: error.message, code: error.code };
     }
   }
 
-  return { message: "Unknown error!" };
+  return { message: "Unknown client error!" };
+};
+
+function isJson(contentType: string | null): boolean {
+  if(!contentType) {
+    return false;
+  }
+
+  return /application\/json/.test(contentType);
 };
