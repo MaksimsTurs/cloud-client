8f0401bbf5fe8d42e7b7c0bff94f0d109c587cbc
MaksimsTurs <maksims07turs@gmail.com>
2026-03-22 14:57:39 +0100

    refactor(page/Home): add header, main and footer to Empty component

--- src/pages/Home/Page.page.tsx
+++ src/pages/Home/Page.page.tsx
@@ -3,29 +3,38 @@ import type { ReactNode } from "react";
 import { Fragment, useEffect } from "react";
 
 import { FileExplorer } from "@feature/file-explorer/file-explorer.feature";
+import { Link } from "@hook/use-react-router/use-react-router.hook";
 import Empty from "@ui/Empty/Empty.component";
-import Title from "@component/Title/Title.component";
-
-import serializeError from "@util/serialize-error.util";
+import Metadata from "@component/Metadata/Metadata.component";
 
 import { useFileExplorerHistory } from "@feature/file-explorer/file-explorer.feature";
-import { useAuth } from "@service/auth/auth.service";
+import { useAuthIsAuthorized, useAuthIsAuthorizing } from "@service/auth/auth.service";
 
 export default function Page(): ReactNode {
   const feHistory = useFileExplorerHistory();
-  const { isAuthorized } = useAuth({ serializeError });
+  const isAuthorized = useAuthIsAuthorized();
+  const isAuthorizing = useAuthIsAuthorizing();
 
   useEffect(() => {
-    if(isAuthorized && !feHistory.hasRoot) {
+    if(!isAuthorizing && isAuthorized && !feHistory.hasRoot) {
       feHistory.open("root");
     }
-  }, [])
+  }, [isAuthorizing, isAuthorized]);
 
   return(
     <Fragment>
-      <Title>File Explorer</Title>
-      {!feHistory.isFetchDirectory && !isAuthorized && !isAuthorized ? 
-      <Empty>You need log in or log up first!</Empty> :
+      <Metadata title="File Explorer"/>
+      {!feHistory.isFetchDirectory && !isAuthorized ? 
+      <Empty
+        header="Not Authenticated!"
+        main="It looks like you either haven't logged into your account, or do not have an account."
+        footer={
+          <Fragment>
+            <Link href="/log-in">Log in</Link>
+            or
+            <Link href="/log-up">Log up</Link>
+          </Fragment>
+        }/> :
       <FileExplorer/>}
     </Fragment>
   );
