87d498ae436dbf347c1da565914a403aff560c22
MaksimsTurs <maksims07turs@gmail.com>
2026-03-22 14:58:50 +0100

    feat: add check functions for mime types

--- src/utils/is.util.ts
+++ src/utils/is.util.ts
@@ -1,4 +1,4 @@
-import type { VoidFunction, Contructable } from "@root/global.type";
+import type { Constructable } from "@root/global.type";
 
 export const isString     = (something: any): something is string => typeof something === "string";
 export const isNumber     = (something: any): something is number => typeof something === "number";
@@ -6,11 +6,11 @@ export const isBoolean    = (something: any): something is boolean => typeof som
 export const isUndefined  = (something: any): something is undefined => something === undefined;
 export const isNull       = (something: any): something is null => something === null;
 export const isArray      = <T = any>(something: any): something is T[] => Array.isArray(something);
-export const isObject     = <T = any>(something: any): something is T => typeof something === "object" && !isArray(something) && !isNull(something);
-export const isFunction   = <T = VoidFunction>(something: any): something is T => typeof something === "function";
+export const isObject     = <T extends Record<any, any>>(something: any): something is T => Object.getPrototypeOf(something) === Object.prototype && !isArray(something);
+export const isFunction   = <T = any>(something: any): something is T => typeof something === "function";
 export const isBigNumber  = (something: any): something is bigint => typeof something === "bigint";
 export const isSymbol     = (something: any): something is symbol => typeof something === "symbol";
-export const isInstanceOf = <T extends Contructable>(maybeInstance: any, Constructor: T): maybeInstance is InstanceType<T> => maybeInstance instanceof Constructor;
+export const isInstanceOf = <T extends Constructable>(maybeInstance: any, Constructor: T): maybeInstance is InstanceType<T> => maybeInstance instanceof Constructor;
 export const isPrimitive  = (something: any): boolean => (
   isString(something)    ||
   isNumber(something)    ||
@@ -20,3 +20,34 @@ export const isPrimitive  = (something: any): boolean => (
   isBigNumber(something) ||
   isSymbol(something)    
 );
+
+const MIME_TYPES = {
+  TEXT:   "text/*",
+  VIDEO:  "video/*",
+  AUDIO:  "audio/*",
+  IMAGE:  "image/*",
+  BINARY: "application/octet-stream"
+};
+
+export const isMimeTypeText = (mimeType: string): boolean => {
+  return(
+    new RegExp(MIME_TYPES.TEXT).test(mimeType) ||
+    mimeType === "application/json"
+  );
+};
+
+export const isMimeTypeVideo = (mimeType: string): boolean => {
+  return new RegExp(MIME_TYPES.VIDEO).test(mimeType);
+};
+
+export const isMimeTypeAudio = (mimeType: string): boolean => {
+  return new RegExp(MIME_TYPES.AUDIO).test(mimeType);
+};
+
+export const isMimeTypeImage = (mimeType: string): boolean => {
+  return new RegExp(MIME_TYPES.IMAGE).test(mimeType);
+};
+
+export const isMimeTypeBinary = (mimeType: string): boolean => {
+  return MIME_TYPES.BINARY === mimeType;
+};
