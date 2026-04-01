833a73586561e99c8a9454a682c8ca29cc8d9b73
MaksimsTurs <maksims07turs@gmail.com>
2026-04-01 14:42:11 +0200

    refactor(File-Explorer): show different empty message depends on is user verified or not

--- src/features/file-explorer/components/File-Explorer/File-Explorer.component.tsx
+++ src/features/file-explorer/components/File-Explorer/File-Explorer.component.tsx
@@ -1,6 +1,7 @@
 import scss from "./File-Explorer.module.scss";
 
 import type { ReactNode } from "react";
+import type { User } from "@root/global.type";
 
 import File from "./component/File.component";
 import FileExplorerLoader from "../File-Explorer-Loader/File-Explorer-Loader.component";
@@ -10,6 +11,7 @@ import ContextMenu from "../Context-Menu/Context-Menu.component";
 import Empty from "@ui/Empty/Empty.component";
 
 import { useFileExplorerHistory, useFileExplorerItemsEvents } from "@feature/file-explorer/file-explorer.feature";
+import { useUser } from "@service/auth/auth.service";
 
 import hasKey from "@feature/file-explorer/utils/has-key.util";
 
@@ -17,6 +19,7 @@ import FE_ITEM_TYPES from "../../const/FE-ITEM-TYPES.const";
 
 export default function FileExplorer(): ReactNode {
   const feHistory = useFileExplorerHistory();
+  const user = useUser<User>();
   const { selected } = useFileExplorerItemsEvents();
 
   if(feHistory.isFetchDirectory) {
@@ -29,13 +32,17 @@ export default function FileExplorer(): ReactNode {
   }
 
   if(!feHistory.items.length) {
+    const emptyMessage: string = user.is_verified ?
+      `Looks like ${feHistory.paths.at(-1)} folder does not have any item!` :
+      `Looks like you does not have verified you e - mail, verify you e - mail first to get access to you folders and files!`;
+
     return(
       <ContextMenu>
         <div className={scss.explorer_container}>
           <BreadCrumbs/>
           <Empty
             header="Folder is Empty!"
-            main={`Looks like ${feHistory.paths.at(-1)} folder does not have any item!`}/>
+            main={emptyMessage}/>
         </div>
       </ContextMenu>
    );
