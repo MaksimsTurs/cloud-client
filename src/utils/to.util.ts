194232ea69b4f1ce1c980dc0a5a62819c2748f87
MaksimsTurs <maksims07turs@gmail.com>
2026-04-01 16:54:00 +0200

    fix: error when trying check value type that is undefined

--- src/utils/to.util.ts
+++ src/utils/to.util.ts
@@ -1,24 +1,53 @@
-import { isInstanceOf, isArray, isObject } from "./is.util";
+import { isArray, isInstanceOf, isObject, isUndefined } from "./is.util";
 
-const appendFormData = (formData: FormData, key: string, value: any): void => {
-  if(isInstanceOf(value, FileList)) {
-    for(let file of value) {
-      formData.append(key, file, file.name);
-    }
-  } else if(isArray(value) || isObject(value)) {
-    formData.append(key, JSON.stringify(value))
-  } else {
-    formData.append(key, value?.toString());
+export function fromObjectToFormData(object?: Record<any, any>): FormData {
+  const formData: FormData = new FormData();
+
+  if(!object) {
+    return formData;
   }
-};
 
-export function toFormDataFromObject<T = unknown>(obj: T): FormData {
-  const formData = new FormData();
-
-  for(let key in obj) {
-    const value: any = obj[key];
-    appendFormData(formData, key, value);
+  for(let key in object) {
+    const value = object[key];
+    
+    if(!isUndefined(value) && isArray(value)) {
+      appendArray(formData, key, value);
+    } else if(!isUndefined(value) && isInstanceOf(value, FileList)) {
+      appendFileList(formData, key, value);
+    } else if(!isUndefined(value) && isObject(value)) {
+      appendObject(formData, key, value);
+    } else {
+      formData.append(key, value);
+    }
   }
 
   return formData;
+};
+
+function appendObject(formData: FormData, prefix: string, object: any): void {
+  for(let name in object) {
+    const value = object[name];
+    const key = `${prefix}.${name}`;
+    
+    if(!isUndefined(value) && isArray(value)) {
+      appendArray(formData, key, value);
+    } else if(!isUndefined(value) && isInstanceOf(value, FileList)) {
+      appendFileList(formData, key, value);
+    } else if(!isUndefined(value) && isObject(value)) {
+      appendObject(formData, key, value);
+    } else {
+      formData.append(key, value);
+    }
+  }
+};
+
+function appendArray(formData: FormData, key: string, value: any[]): void {
+  formData.append(key, JSON.stringify(value));
+};
+
+function appendFileList(formData: FormData, key: string, value: FileList): void {
+  for(let index: number = 0; index < value.length; index++) {
+    const file: File = value.item(index)!;
+    formData.append(key, file, file.name);
+  }
 };
