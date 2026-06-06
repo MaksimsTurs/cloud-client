2a0e51d94fa7fa178ad365468cd363b913e5fc95
MaksimsTurs <maksims07turs@gmail.com>
2026-06-06 14:43:50 +0200

    feat: add content to error boundary component

--- src/components/Error-Boundary/Error-Boundary.component.tsx
+++ src/components/Error-Boundary/Error-Boundary.component.tsx
@@ -1,27 +1,51 @@
-// import scss from "./Error-Boundary.module.scss";
+import type { ReactNode } from "react";
+import type { ErrorBoundaryState } from "./Error-Boundary.component.type";
+
+import scss from "./Error-Boundary.module.scss";
 
 import { Fragment, Component } from "react";
+import { BugIcon } from "lucide-react";
 
-import type { ReactNode } from "react";
-import type { ErrorBoundaryProps, ErrorBoundaryState } from "./Error-Boundary.component.type";
+import Metadata from "@component/Metadata/Metadata.component";
+import TextButton from "@ui/Text-Button/Text-Button.component";
 
-class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
-  constructor(props: ErrorBoundaryProps) {
-    super(props);
-    this.state = { hasError: false };
-  };
-
+class ErrorBoundary extends Component<unknown, ErrorBoundaryState> {
   public static getDerivedStateFromError(_: Error): ErrorBoundaryState {
     return { hasError: true };
   };
 
-  public componentDidCatch(error: Error): void {
-    console.info(`Error: ${error.message}`);
+  public constructor(props: unknown) {
+    super(props);
+
+    this.state = {
+      hasError: false 
+    };
   };
 
-  render(): ReactNode {
+  public componentDidCatch(_error: Error): void {};
+
+  public goToHomePage(): void {
+    window.open("/", "_self");
+  };
+
+  public render(): ReactNode {
     return(
-      <Fragment>{this.state.hasError ? this.props.fallbackComponent : this.props.children}</Fragment>
+      <Fragment>
+        <Metadata title="Error is occur"/>
+        {!this.state.hasError ? this.props.children : 
+        <div className={scss.error_container}>
+          <div className={scss.error_body}>
+            <div className={scss.error_header}>
+              <BugIcon size={32} strokeWidth={1.5}/>
+              <h3>Unexpected error!</h3>
+            </div>
+            <div className={scss.error_information}>
+              <p>Automatic report has ben sended to developer, go to the home page or try do you stuff later.</p>
+              <TextButton onClick={this.goToHomePage.bind(this)} text="Go Home"/>
+            </div>
+          </div>
+        </div>}
+      </Fragment>
     )
   };
 };
--- src/components/Error-Boundary/Error-Boundary.component.type.ts
+++ src/components/Error-Boundary/Error-Boundary.component.type.ts
@@ -1,9 +1,3 @@
-import type { PropsWithChildren, ReactNode } from "react";
-
-export type ErrorBoundaryProps = PropsWithChildren<{
-  fallbackComponent?: ReactNode
-}>;
-
 export type ErrorBoundaryState = {
   hasError: boolean
 };
